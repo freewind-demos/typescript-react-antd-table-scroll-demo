726b042e9dc3f0203c1165696f3d3ec05ad954ed
Freewind <nowindlee@gmail.com>
2024-01-16 20:39:22 +0800

    typescript-react-antd-table-scroll-demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript React AntDesign Table Demo
+TypeScript React AntDesign Table Scroll Demo
 =================================
+
+只要设置了 `scroll` 就表示要滚动，此时table本身的大小就像一个窗口，内部通过 `x/y` 来设置下面内容的真实宽和高。
+
+其中scroll.x/y除了支持数字（像素）外，也支持`vh/vw`
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -2,51 +2,40 @@ import React from 'react'
 import {Table} from 'antd';
 
 const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: 'Cash Assets',
-    className: 'column-money',
-    dataIndex: 'money',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-  },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        render: (text: string) => <a>{text}</a>,
+    },
+    {
+        title: 'Cash Assets',
+        className: 'column-money',
+        dataIndex: 'money',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+    },
 ];
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    money: '￥300,000.00',
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    money: '￥1,256,000.00',
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    money: '￥120,000.00',
-    address: 'Sidney No. 1 Lake Park',
-  },
-];
+const data = new Array(20).fill(1).map((_, index) => ({
+    key: `key-${index}`,
+    name: `John Brown-$(index)`,
+    money: `￥300,000.00-$(index)`,
+    address: `New York No. 1 Lake Park-$(index)`,
+
+}));
 
 export default function Hello() {
-  return <div>
-    <Table
-      columns={columns}
-      dataSource={data}
-      bordered
-      title={() => 'Header'}
-      footer={() => 'Footer'}
-    />
-  </div>
+    return <div>
+        <Table
+            style={{width: 600}}
+            scroll={{x: 800, y: '50vh'}}
+            columns={columns}
+            dataSource={data}
+            bordered
+            title={() => 'Header'}
+            footer={() => 'Footer'}
+        />
+    </div>
 };
